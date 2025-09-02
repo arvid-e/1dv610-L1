39b8560f7bc672d2446e995adf1dbf756a2d4a35
arvid-e <arvid.edvinsson@gmail.com>
2025-09-02 10:49:42 +0900

    Add simple greeting

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('What is your name? ', (name: string) => {
+  console.log(`Hello ${name}!`)
+
+  process.exit(0);
+});
